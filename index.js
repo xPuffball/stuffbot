04b8f5242f86712616efd98cc148eb7443ab78a8
Jaemin Han <j4emin.han@gmail.com>
2021-10-28 18:57:14 -0400

    create messaging arguments

--- index.js
+++ index.js
@@ -1,25 +1,21 @@
-const express = require('express');
-const { port } = require('./config.json');
-
-const app = express();
-
 const dotenv = require('dotenv')
 const result = dotenv.config({path: 'process.env'})
 const { Client, Intents } = require('discord.js');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
-
-app.get('/', (request, response) => {
-	return response.sendFile('index.html', { root: '.' });
-});
-
-app.listen(port, () => console.log(`App listening at http://localhost:${port}`));
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
 client.once('ready', () => {
     console.log('Ready!');
 })
 
-client.on('message', msg => {
-    msg.reply('hiyo')
+client.on('messageCreate', msg => {
+    if (msg.author.bot) {
+        return false;
+    } else {
+        if (msg.content.toLowerCase().includes('stuff')) {
+            msg.reply(`i'm stuff`)
+        }
+    }
+    
 });
 
 client.login(process.env.TOKEN);
